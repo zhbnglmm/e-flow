75a81b119f04be53174d6096074bb3859d40888c
zhbnglmm <tjuzhang@gmail.com>
2024-06-16 21:44:27 +0800

    flask

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print("hehe")
-pr
+from flask import Flask
+
+# 创建一个 Flask 应用
+app = Flask(__name__)
+
+# 定义一个路由，当访问根路径时返回 "Hello, World!"
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+# 启动应用
+if __name__ == '__main__':
+    app.run()
